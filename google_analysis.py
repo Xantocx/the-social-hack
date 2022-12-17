40a915a05c154071e560e42a80a9aa3c126c665f
Maximilian Mayer <maximilian_mayer@gmx.net>
2022-12-17 19:42:53 +0100

    updated test for google search

--- google_analysis.py
+++ google_analysis.py
@@ -4,7 +4,7 @@ from NewsTracker.Google import GoogleSearch
 config = Configuration.load_from(".env")
 
 reddit_search = GoogleSearch(config, "reddit.com")
-results = reddit_search.search("tesla", num=56)
+results = reddit_search.search("tesla", num=100)
 
 print("\n\n")
 for result in results:
