e405554e5f0f0d69830e3ac55c55dfbff873a9fc
Maximilian Mayer <maximilian_mayer@gmx.net>
2022-12-18 01:27:33 +0100

    added some analysis code

--- NewsTracker/Twitter.py
+++ NewsTracker/Twitter.py
@@ -6,15 +6,19 @@ import tweepy
 import twint
 import json
 import sys
+import shutil
+import os
+import matplotlib.pyplot as plt
 
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from enum import Enum
 from json import JSONEncoder
 from datetime import datetime, timedelta
-from io import StringIO 
+from io import StringIO
+from math import ceil
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from pandas import DataFrame
+from pandas import DataFrame, concat
 from tqdm import tqdm
 
 
@@ -23,6 +27,8 @@ from tqdm import tqdm
 
 
 class TwintCapturing(list):
+
+    RESUME_LOG_FILE = "./temporary_resume_log.txt"
 
     def __enter__(self):
         self._stdout = sys.stdout
@@ -35,16 +41,33 @@ class TwintCapturing(list):
         sys.stdout = self._stdout
 
     @classmethod
-    def search(cls, config: twint.Config):
+    def fast_search(cls, config: twint.Config) -> DataFrame:
         config.Pandas = True
-
         with cls() as _:
             try:
                 twint.run.Search(config)
             except:
                 return DataFrame()
-
         return twint.output.panda.Tweets_df
+
+    @classmethod
+    def search(cls, config: twint.Config, repetitions: int = 1) -> DataFrame:
+        if repetitions < 2: return cls.fast_search(config)
+
+        config.Pandas = True
+        config.Resume = cls.RESUME_LOG_FILE
+
+        results = [DataFrame()]
+        for _ in range(repetitions):
+            with cls() as _:
+                try:
+                    twint.run.Search(config)
+                except:
+                    break
+            results.append(twint.output.panda.Tweets_df)
+
+        if os.path.exists(cls.RESUME_LOG_FILE): os.remove(cls.RESUME_LOG_FILE)
+        return concat(results)
 
 
 class TwitterAnalyzer:
@@ -63,20 +86,24 @@ class TwitterAnalyzer:
                             "Replies", 
                             "Retweets", 
                             "Quotes",
+                            "Engagement Score",
                             "Negative Sentiment Score",
                             "Neutral Sentiment Score",
                             "Positive Sentiment Score",
                             "Compound Sentiment Score"]
 
+
         class TweetEncoder(JSONEncoder):
             def default(self, obj):
                 return obj.__dict__ 
+
 
         class Origin(Enum):
             SEARCH = "search"
             REPLY = "reply"
             RETWEET = "retweet"
             QUOTE = "quote"
+
 
         class Stats:
 
@@ -90,7 +117,7 @@ class TwitterAnalyzer:
                                negative_sentiment:  bool = None,
                                neutral_sentiment:   bool = None,
                                positive_sentiment:  bool = None,
-                               coupound_sentiment:  bool = None,
+                               compound_sentiment:  bool = None,
                                request_reply_count: bool = False,
                                request_quote_count: bool = True) -> None:
 
@@ -109,7 +136,7 @@ class TwitterAnalyzer:
                 self.negative_sentiment = negative_sentiment
                 self.neutral_sentiment  = neutral_sentiment
                 self.positive_sentiment = positive_sentiment
-                self.coupound_sentiment = coupound_sentiment
+                self.compound_sentiment = compound_sentiment
 
             @property
             def engagement_score(self) -> int:
@@ -117,8 +144,8 @@ class TwitterAnalyzer:
 
             @property
             def is_positive(self) -> bool:
-                if self.coupound_sentiment:
-                    return self.coupound_sentiment > 0
+                if self.compound_sentiment:
+                    return self.compound_sentiment > 0
                 return None
 
             def sentiment_analysis(self, text: str) -> None:
@@ -126,7 +153,7 @@ class TwitterAnalyzer:
                 self.negative_sentiment = polarity["neg"]
                 self.neutral_sentiment = polarity["neu"]
                 self.positive_sentiment = polarity["pos"]
-                self.coupound_sentiment = polarity["compound"]
+                self.compound_sentiment = polarity["compound"]
 
             @classmethod
             def from_dict(cls, stats_dict):
@@ -138,7 +165,7 @@ class TwitterAnalyzer:
                     negative_sentiment  = stats_dict["negative_sentiment"],
                     neutral_sentiment   = stats_dict["neutral_sentiment"],
                     positive_sentiment  = stats_dict["positive_sentiment"],
-                    coupound_sentiment  = stats_dict["coupound_sentiment"],
+                    compound_sentiment  = stats_dict["compound_sentiment"],
                     request_reply_count = stats_dict["request_reply_count"],
                     request_quote_count = stats_dict["request_quote_count"]
                 )
@@ -164,6 +191,7 @@ class TwitterAnalyzer:
                 )
                 stats.sentiment_analysis(status.text)
                 return stats
+
 
         def __init__(self, tweet_id: str, 
                            conversation_id: str, 
@@ -218,10 +246,11 @@ class TwitterAnalyzer:
                 self.stats.reply_count,
                 self.stats.retweet_count,
                 self.stats.quote_count,
+                self.stats.engagement_score,
                 self.stats.negative_sentiment,
                 self.stats.neutral_sentiment,
                 self.stats.positive_sentiment,
-                self.stats.coupound_sentiment
+                self.stats.compound_sentiment
             ]
 
         def add_replies(self, tweets: List) -> None:
@@ -296,6 +325,88 @@ class TwitterAnalyzer:
             return tweets
 
 
+    class User:
+
+        DATAFRAME_COLUMS = ["Username", 
+                            "Followers", 
+                            "Tweet Count", 
+                            "Likes", 
+                            "Replies", 
+                            "Retweets",
+                            "Quotes",
+                            "Engagement Score",
+                            "Negative Sentiment Score",
+                            "Neutral Sentiment Score",
+                            "Positive Sentiment Score",
+                            "Compound Sentiment Score"]
+
+        def __init__(self, username: str, followers: int) -> None:
+            self.username  = username
+            self.followers = followers
+
+            self.tweets   = []
+
+        @property
+        def tweet_count(self) -> int:
+            return len(self.tweets)
+
+        @property
+        def likes(self) -> int:
+            return sum(tweet.stats.like_count for tweet in self.tweets)
+
+        @property
+        def replies(self) -> int:
+            return sum(tweet.stats.reply_count for tweet in self.tweets)
+
+        @property
+        def retweets(self) -> int:
+            return sum(tweet.stats.retweet_count for tweet in self.tweets)
+
+        @property
+        def quotes(self) -> int:
+            return sum(tweet.stats.quote_count for tweet in self.tweets)
+
+        @property
+        def engagement_score(self) -> int:
+            return (self.likes + 2 * self.retweets + 3 * self.replies + 4 * self.quotes) / 10
+
+        @property
+        def negative_sentiment(self) -> float:
+            return sum(tweet.stats.negative_sentiment for tweet in self.tweets) / self.tweet_count
+
+        @property
+        def neutral_sentiment(self) -> float:
+            return sum(tweet.stats.neutral_sentiment for tweet in self.tweets) / self.tweet_count
+
+        @property
+        def positive_sentiment(self) -> float:
+            return sum(tweet.stats.neutral_sentiment for tweet in self.tweets) / self.tweet_count
+
+        @property
+        def compound_sentiment(self) -> float:
+            return sum(tweet.stats.compound_sentiment for tweet in self.tweets) / self.tweet_count
+
+        @property
+        def dataframe_row(self) -> List:
+            return [
+                self.username,
+                self.followers,
+                self.tweet_count,
+                self.likes,
+                self.replies,
+                self.retweets,
+                self.quotes,
+                self.engagement_score,
+                self.negative_sentiment,
+                self.neutral_sentiment,
+                self.positive_sentiment,
+                self.compound_sentiment
+            ]
+
+        def add_tweet(self, tweet) -> None:
+            self.tweets.append(tweet)
+
+
     def __init__(self, config: Configuration) -> None:
         self.config = config
         token = config.twitter_bearer_token
@@ -306,17 +417,88 @@ class TwitterAnalyzer:
 
         self.google = GoogleSearch(self.config, "twitter.com")
 
+    def analyze_url(self, url: str, foldername: str) -> None:
+        os.makedirs(os.path.dirname(foldername), exist_ok=True)
+        parent_tweets, all_tweets = self.store_tweets_for_url(url, os.path.join(foldername, "tweets.json"))
+        self.analyze(parent_tweets, all_tweets, foldername)
+
+    def analyze_tweets_file(self, tweets_file: str, foldername: str) -> None:
+
+        parent_tweets, all_tweets = self.load_tweets_from_file(tweets_file)
+
+        # make sure tweets json file is in target folder as well
+        source_file = os.path.abspath(tweets_file)
+        target_file = os.path.abspath(os.path.join(foldername, "tweets.json"))
+        if source_file != target_file:
+            os.makedirs(os.path.dirname(foldername), exist_ok=True)
+            shutil.copy(source_file, target_file)
+
+        self.analyze(parent_tweets, all_tweets, foldername)
+        
+    def analyze(self, parent_tweets: List[Tweet], all_tweets: List[Tweet], foldername: str) -> None:
+
+        def filename(filename: str) -> str: return os.path.join(foldername, filename)
+
+        users = self.get_users_from_tweets(all_tweets)
+
+        # only temporarily disabled for testing!
+        # self.tweets_to_csv(all_tweets, filename("tweets.csv"))
+        # self.users_to_csv(users, filename("users.csv"))
+
+        # tweet origin
+        origins   = [tweet._origin for tweet in all_tweets]
+        origins_x = [origin.value for origin in TweetOrigin]
+        origins_y = [len(list(filter(lambda tweet_origin: tweet_origin == origin, origins))) for origin in origins_x]
+        plt.bar(origins_x, origins_y)
+        plt.xlabel('Origin')
+        plt.ylabel('Number of Tweets')
+        plt.savefig(filename("origin.png"))
+        plt.clf()
+        
+        # compound sentiment visualization
+        compound_sentiment = [tweet.stats.compound_sentiment for tweet in all_tweets]
+        compound_x = [x/10 for x in range(-10, 11)]
+        compound_y = [len(list(filter(lambda sentiment: x-0.05 <= sentiment < x+0.05, compound_sentiment))) for x in compound_x]
+        plt.plot(compound_x, compound_y)
+        plt.xlabel('Compound Sentiment')
+        plt.ylabel('Number of Tweets')
+        plt.savefig(filename("compound_sentiment.png"))
+        plt.clf()
+
+        # based on sentiment, how high is engagement score
+        compound_x = [x/10 for x in range(-10, 11)]
+        tweets_by_sentiment = [list(filter(lambda tweet: x-0.05 <= tweet.stats.compound_sentiment < x+0.05, all_tweets)) for x in compound_x]
+        engagement_y = [sum(tweet.stats.engagement_score for tweet in tweets) for tweets in tweets_by_sentiment]
+        plt.plot(compound_x, engagement_y)
+        plt.xlabel('Compound Sentiment')
+        plt.ylabel('Combined Engagement')
+        plt.savefig(filename("engagement_vs_sentiment.png"))
+        plt.clf()
+
+        # based on sentiment, how high is the average engagement score
+        compound_x = [x/10 for x in range(-10, 11)]
+        tweets_by_sentiment = [list(filter(lambda tweet: x-0.05 <= tweet.stats.compound_sentiment < x+0.05, all_tweets)) for x in compound_x]
+        engagement_y = [sum(tweet.stats.engagement_score for tweet in tweets) / max(len(tweets), 1) for tweets in tweets_by_sentiment]
+        plt.plot(compound_x, engagement_y)
+        plt.xlabel('Compound Sentiment')
+        plt.ylabel('Average Engagement')
+        plt.savefig(filename("avg_engagement_vs_sentiment.png"))
+        plt.clf()
+
+        # sentiment by origin
+        origins_x = [origin.value for origin in TweetOrigin]
+        tweets_by_origin = [list(filter(lambda tweet: tweet._origin == origin, all_tweets)) for origin in origins_x]
+        sentiment_y = [sum(tweet.stats.compound_sentiment for tweet in tweets) / max(len(tweets), 1) for tweets in tweets_by_origin]
+        plt.bar(origins_x, sentiment_y)
+        plt.xlabel('Origin')
+        plt.ylabel('Compound Sentiment')
+        plt.savefig(filename("sentiment_by_origin.png"))
+        plt.clf()
+
     def store_tweets_for_url(self, url: str, filename: str, limit: int = 1000, recusion_depth = 0) -> Tuple[List[Tweet], List[Tweet]]:
         parent_tweets, all_tweets = self.get_tweets_for_url(url, limit, recusion_depth)
-        with open(filename, "w") as file:
-            json.dump(parent_tweets, file, cls=TweetEncoder)
+        self.store_tweets(parent_tweets, filename)
         return parent_tweets, all_tweets
-
-    def load_tweets_from_file(self, filename: str) -> Tuple[List[Tweet], List[Tweet]]:
-        with open(filename, "r") as file:
-            json_list = json.load(file)
-        tweets = [Tweet.from_dict(tweet) for tweet in json_list]
-        return tweets, self.expand_parent_tweets(tweets)
 
     def get_tweets_for_url(self, url: str, limit: int = 1000, recusion_depth = 0) -> Tuple[List[Tweet], List[Tweet]]:
         print("Searching tweets with URL...", end=" ")
@@ -343,19 +525,45 @@ class TwitterAnalyzer:
         # for result in google_search:
         #     print(f"{result.title}: {result.url}")
 
+    def get_users_from_tweets(self, tweets: List[Tweet]) -> Dict[str, User]:
+        usernames = list(set([tweet.username for tweet in tweets]))
+        requested_users = len(usernames)
+        required_requests = ceil(requested_users / 100) if len(usernames) > 100 else 1
+
+        user_info = []
+        request_round = 0
+        while request_round < required_requests:
+            user_batch = usernames[request_round*100:min(request_round*100+100, requested_users)]
+            user_info += self.api.lookup_users(screen_name=user_batch)
+            request_round += 1
+
+        users = {user.screen_name: TwitterUser(user.screen_name, user.followers_count) for user in user_info}
+
+        for tweet in tweets:
+            users[tweet.username].add_tweet(tweet)
+
+        return users
+
+
     def new_twint_config(self, hide_output: bool = True) -> twint.Config:
         c = twint.Config()
         c.Hide_output = hide_output
         return c
 
-    def search(self, search_term: str, limit: int = 10, since: str = None, hide_output: bool = True) -> List[Tweet]:
+    def search(self, search_term: str, 
+                     limit: int = 1000, 
+                     since: str = None, 
+                     repetitions: int = 10,
+                     origin: Tweet.Origin = Tweet.Origin.SEARCH, 
+                     hide_output: bool = True) -> List[Tweet]:
+
         c = self.new_twint_config(hide_output)
         c.Search = search_term
         c.Limit = limit
         if since: c.Since = since
 
-        results = TwintCapturing.search(c)
-        return Tweet.parse_twint(results, TweetOrigin.SEARCH)
+        results = TwintCapturing.search(c, repetitions)
+        return Tweet.parse_twint(results, origin)
 
     def get_replies(self, tweet: Tweet, limit: int = 1000) -> List[Tweet]:
         c = self.new_twint_config()
@@ -390,8 +598,8 @@ class TwitterAnalyzer:
         return Tweet.parse_tweepy(results, TweetOrigin.RETWEET)
 
     def get_quotes_twint(self, tweet: Tweet, limit: int = 1000) -> List[Tweet]:
-        return self.search(f"url:{tweet.tweet_id}", limit=limit)
-        # return self.search(f"url:{tweet.tweet_id}", limit=limit, since=tweet.search_date)
+        return self.search(f"url:{tweet.tweet_id}", limit=limit, origin=TweetOrigin.QUOTE, repetitions=1)
+        # return self.search(f"url:{tweet.tweet_id}", limit=limit, since=tweet.search_date, origin=TweetOrigin.QUOTE, repetitions=1)
     
     def get_quotes_tweepy(self, tweet: Tweet, limit: int = 10) -> List[Tweet]:
         # use twint instead, this is rate limited as hell
@@ -436,6 +644,16 @@ class TwitterAnalyzer:
             children += tweet.childern
         return parent_tweets + self.expand_parent_tweets(children)
     
+    def store_tweets(self, parent_tweets: List[Tweet], filename: str) -> None:
+        with open(filename, "w") as file:
+            json.dump(parent_tweets, file, cls=TweetEncoder)
+
+    def load_tweets_from_file(self, filename: str) -> Tuple[List[Tweet], List[Tweet]]:
+        with open(filename, "r") as file:
+            json_list = json.load(file)
+        tweets = [Tweet.from_dict(tweet) for tweet in json_list]
+        return tweets, self.expand_parent_tweets(tweets)
+
     def tweets_to_dataframe(self, tweets: List[Tweet]) -> DataFrame:
         data = [tweet.dataframe_row for tweet in tweets]
         return DataFrame(data, columns=Tweet.DATAFRAME_COLUMS)
@@ -444,8 +662,19 @@ class TwitterAnalyzer:
         df = self.tweets_to_dataframe(tweets)
         df.to_csv(filename)
 
+    def users_to_dataframe(self, users: Dict[str, User]) -> DataFrame:
+        data = [user.dataframe_row for user in users.values()]
+        return DataFrame(data, columns=TwitterUser.DATAFRAME_COLUMS)
+
+    def users_to_csv(self, users: Dict[str, User], filename: str) -> None:
+        df = self.users_to_dataframe(users)
+        df.to_csv(filename)
+
 
 Tweet = TwitterAnalyzer.Tweet
+Tweets = List[Tweet]
 TweetOrigin = Tweet.Origin
 TweetStats = Tweet.Stats
 TweetEncoder = Tweet.TweetEncoder
+TwitterUser = TwitterAnalyzer.User
+TwitterUsers = List[TwitterUser]
--- twitter_analysis.py
+++ twitter_analysis.py
@@ -3,6 +3,9 @@ from NewsTracker.Twitter import *
 config = Configuration.load_from(".env")
 
 twitter = TwitterAnalyzer(config)
-parents, all = twitter.store_tweets_for_url("https://www.nytimes.com/2022/12/15/business/china-zero-covid-apology.html", "./demo_set.json", 1000, 1)
-# parents, all = twitter.load_tweets_from_file("./test.json")
-twitter.tweets_to_csv(all, "./demo_set.csv")
+# parents, all = twitter.store_tweets_for_url("https://www.nytimes.com/2022/12/15/business/china-zero-covid-apology.html", "./demo_set.json", 1000, 1)
+# twitter.tweets_to_csv(all, "./demo_set.csv")
+
+twitter.analyze_url("url:https://music.apple.com/us/album/faith-in-the-future-deluxe/1640572729", "./sude_tweet/")
+
+# twitter.analyze_tweets_file("./twitter-testing/tweets.json", "./twitter-testing/")
